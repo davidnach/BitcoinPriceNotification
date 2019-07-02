15d743c6f80bd167f0faeb53d0e0de8a6863cb28
David <davidnachson@gmail.com>
2019-07-02 13:42:52 -0700

    version1

--- app.js
+++ app.js
@@ -1,9 +1,35 @@
 const express = require('express')
 const path = require('path');
 const router = express.Router();
-
+const bodyParser = require('body-parser');
 const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
 const port = (process.env.PORT || 3000);
+
+const AWS = require("aws-sdk");
+
+AWS.config.update({
+	region: "us-east-2"
+});
+
+
+var dynamodb = new AWS.DynamoDB();
+
+var params = {
+	TableName: "EmailSubscriber",
+	Item: {
+		"emailAddress":{
+			S: "davidnachson@gmail.com"
+		},
+		"name": {
+			S: "david"
+		}
+	}
+};
+
+
+
 
 
 app.use('/public',express.static('public'));
@@ -11,7 +37,20 @@ app.use('/public',express.static('public'));
 
 router.get('/', (req, res) => res.sendFile(path.join(__dirname,'index.html')));
 
-router.get('/about', (req, res) => res.send("Hello world!"));
+
+router.post('/submitForm', function(req,res) {
+	dynamodb.putItem(params, function(err,data) {
+		if(err){
+			res.status(err).end();
+			console.log('DDB error: ' + err);
+		} else {
+
+			res.status(201).end();
+		}
+	});
+	console.log(req.body);
+});
+	
 
 app.use('/',router);
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
